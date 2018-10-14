d5bb13016fd9a4ae2c666d93f9983d3c291d905f
jiexunsee <jxsee94@yahoo.com.sg>
2018-10-14 16:09:02 +0800

    added number of parameters print

--- GCMC adaptation/train.py
+++ GCMC adaptation/train.py
@@ -436,6 +436,20 @@ wait = 0
 
 print('Training...')
 
+total_parameters = 0
+for variable in tf.trainable_variables():
+    # shape is an array of tf.Dimension
+    shape = variable.get_shape()
+    # print(shape)
+    # print(len(shape))
+    variable_parameters = 1
+    for dim in shape:
+        # print(dim)
+        variable_parameters *= dim.value
+    # print(variable_parameters)
+    total_parameters += variable_parameters
+print('Total params: {}'.format(total_parameters))
+
 for epoch in range(NB_EPOCH):
 
     t = time.time()
